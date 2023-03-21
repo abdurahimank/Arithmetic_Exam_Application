3d471597e4a8a22f7189bc42a7b22fd63e57194b
ABDURAHIMAN K <laluvga@gmail.com>
2023-03-21 17:45:33 +0530

    Stage 3/4

--- stages.py
+++ stages.py
@@ -1,10 +1,23 @@
-# Stage 2/4: Task generator
+# Stage 3/4: More tasks needed!
 import random
 
 
-x, y, oper = random.randint(2, 9), random.randint(2, 9), random.choice(["+", "-", "*"])
-print(x, oper, y)
-if int(input()) == eval(str(x) + oper + str(y)):
-    print("Right!")
-else:
-    print("Wrong!")
+game_count = 0
+total_mark = 0
+while game_count < 5:
+    game_count += 1
+    x, y, oper = random.randint(2, 9), random.randint(2, 9), random.choice(["+", "-", "*"])
+    print(x, oper, y)
+    while True:
+        try:
+            answer = int(input())
+            break
+        except (ValueError, TypeError):
+            print("Incorrect format.")
+    if answer == eval(str(x) + oper + str(y)):
+        print("Right!")
+        total_mark += 1
+    else:
+        print("Wrong!")
+print(f"Your mark is {total_mark}/5.")
+
