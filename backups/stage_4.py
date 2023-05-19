ec11f20c571e43d346a2adf445b2d2fbb2a9200e
ABDURAHIMAN K <laluvga@gmail.com>
2023-05-19 16:53:17 +0530

    Stage 4/4: Adding marks

---
+++ backups/stage_4.py
@@ -0,0 +1,67 @@
+# Project: Arithmetic Exam Application
+# Stage 4/4: Adding marks
+import random
+
+
+def level_1():
+    game_count = 0
+    score = 0
+    while game_count < 5:
+        game_count += 1
+        x, y, oper = random.randint(2, 9), random.randint(2, 9), random.choice(["+", "-", "*"])
+        print(x, oper, y)
+        while True:
+            try:
+                answer = int(input())
+                break
+            except (ValueError, TypeError):
+                print("Wrong format! Try again.")
+        if answer == eval(str(x) + oper + str(y)):
+            print("Right!")
+            score += 1
+        else:
+            print("Wrong!")
+    return score
+
+
+def level_2():
+    game_count = 0
+    score = 0
+    while game_count < 5:
+        game_count += 1
+        x = random.randint(11, 29)
+        print(x)
+        while True:
+            try:
+                answer = int(input())
+                break
+            except (ValueError, TypeError):
+                print("Wrong format! Try again.")
+        if answer == x ** 2:
+            print("Right!")
+            score += 1
+        else:
+            print("Wrong!")
+    return score
+
+
+while True:
+    level = input("""Which level do you want? Enter a number:
+1 - simple operations with numbers 2-9
+2 - integral squares of 11-29""")
+    if level == "1":
+        total_mark = level_1()
+    elif level == "2":
+        total_mark = level_2()
+    else:
+        print("Incorrect format.")
+        continue
+    break
+to_save = input(f"Your mark is {total_mark}/5. Would you like to save the result? Enter yes or no.")
+if to_save in ["yes", "YES", "y", "Yes"]:
+    name = input("What is your name?")
+    file = open("results.txt", "at")
+    file.write(f"{name}: {total_mark}/5 in level {level} "
+               f"({'simple operations with numbers 2-9' if level == '1' else 'integral squares 11-29'})")
+    file.close()
+    print('The results are saved in "results.txt".')
--- main.py
+++ main.py
@@ -1,44 +1,65 @@
 import random
 
-mark = 0
-while True:
-    level = input('''Which level do you want? Enter a number:
-1 - simple operations with numbers 2-9
-2 - integral squares of 11-29\n''')
-    if level == '1' or level == '2':
-        break
-    else:
-        print('Incorrect format.')
-for _ in range(5):
-    if level == '1':
-        value_1 = random.randint(2, 9)
-        value_2 = random.randint(2, 9)
-        operation = random.choice(['*', '+', '-'])
-        print(str(value_1) + operation + str(value_2))
-        correct_answer = eval(str(value_1) + operation + str(value_2))
-        description = 'level 1 (simple operations with numbers 2-9)'
-    if level == '2':
-        value_1 = random.randint(11, 29)
-        print(value_1)
-        correct_answer = value_1 ** 2
-        description = 'level 2 (integral squares 11-29)'
 
-    while True:
-        try:
-            player_answer = int(input())
-            break
-        except ValueError:
-            print('Incorrect format.')
-            continue
-    if player_answer == correct_answer:
-        print('Right!')
-        mark += 1
+def level_1():
+    game_count = 0
+    score = 0
+    while game_count < 5:
+        game_count += 1
+        x, y, oper = random.randint(2, 9), random.randint(2, 9), random.choice(["+", "-", "*"])
+        print(x, oper, y)
+        while True:
+            try:
+                answer = int(input())
+                break
+            except (ValueError, TypeError):
+                print("Wrong format! Try again.")
+        if answer == eval(str(x) + oper + str(y)):
+            print("Right!")
+            score += 1
+        else:
+            print("Wrong!")
+    return score
+
+
+def level_2():
+    game_count = 0
+    score = 0
+    while game_count < 5:
+        game_count += 1
+        x = random.randint(11, 29)
+        print(x)
+        while True:
+            try:
+                answer = int(input())
+                break
+            except (ValueError, TypeError):
+                print("Wrong format! Try again.")
+        if answer == x ** 2:
+            print("Right!")
+            score += 1
+        else:
+            print("Wrong!")
+    return score
+
+
+while True:
+    level = input("""Which level do you want? Enter a number:
+1 - simple operations with numbers 2-9
+2 - integral squares of 11-29""")
+    if level == "1":
+        total_mark = level_1()
+    elif level == "2":
+        total_mark = level_2()
     else:
-        print('Wrong!')
-save = input(f'Your mark is {mark}/5. Would you like to save the result? Enter yes or no.\n')
-if save in ['yes', 'YES', 'y', 'Yes']:
-    name = input('What is your name?\n')
-    file = open('results.txt', 'a', encoding='utf_8')
-    file.write(f'{name}: {mark}/5 in {description}.')
+        print("Incorrect format.")
+        continue
+    break
+to_save = input(f"Your mark is {total_mark}/5. Would you like to save the result? Enter yes or no.")
+if to_save in ["yes", "YES", "y", "Yes"]:
+    name = input("What is your name?")
+    file = open("results.txt", "at")
+    file.write(f"{name}: {total_mark}/5 in level {level} "
+               f"({'simple operations with numbers 2-9' if level == '1' else 'integral squares 11-29'})")
     file.close()
     print('The results are saved in "results.txt".')
